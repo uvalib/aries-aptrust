530075f7ed3087a10d54748103b4adf09bf0168c
Dave Goldstein <dpg3k@virginia.edu>
2020-09-02 10:36:56 -0400

    Multi-layer build. Update to Alpine 3.12. Include packaging assets in the source tree

---
+++ package/scripts/entry.sh
@@ -0,0 +1,8 @@
+# run application
+
+# run from here, since application expects json template in templates/
+cd bin; ./aries-aptrust -aptrust $ARIES_APTRUST_URL -apiuser $ARIES_APTRUST_USER -apikey $ARIES_APTRUST_KEY
+
+#
+# end of file
+#
